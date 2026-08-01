4e2fb36de4f986db77a4b0eeebb7a8be6baaa9fc
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-08-01 12:11:31 +0200

    feat: added 2 classes, updated main loop

--- main.py
+++ main.py
@@ -20,23 +20,95 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
 
+class Ship:
+    COOLDOWN = 30
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+    def get_width(self):
+        return self.ship_img.get_width()
+    def get_height(self):
+        return self.ship_img.get_height()
+
+
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 15))
+        pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health / self.max_health), 15))
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+                
+
 
 def main():
     run = True
     FPS = 60
+    player = Player(300, 500)
+    wave_lenght = 0
+    level = 0
+    lives = 5
+    player_vel = 5
     main_font = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 70)
     clock = pygame.time.Clock()
     lost = False
     lost_count = 0
     
+    def redraw_window():
+        WIN.blit(BG, (0, 0))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (610, 10))
+        player.draw(WIN)
+        if lost:
+            lost_label = lost_font.render("You Lost!", 1, (255, 255, 255))
+            WIN.blit(lost_label, (WIDTH / 2 - lost_label.get_width() / 2, HEIGHT / 2 - lost_label.get_height() / 2))
+        pygame.display.update()
     
     while run:
-        clock.tick(FPS)        
+        clock.tick(FPS)
+        redraw_window()
+        if lives <= 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+            else:
+                continue
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
-                
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x - player_vel >= 0:
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player.get_width() + player_vel <= WIDTH:
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel >= 0:
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player.get_height() + 20 + player_vel <= HEIGHT:
+            player.y += player_vel
+
+
 
 
 main()
