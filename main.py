8a3894ce2954d760127e8d53fc12acf73fa30274
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-08-01 17:12:03 +0200

    feat: added laser class

--- main.py
+++ main.py
@@ -21,6 +21,21 @@ pygame.display.set_caption("Space Invaders")
 
 
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+    def move(self, vel):
+        self.y += vel
+    def off_screen(self, height):
+        return self.y > height or self.y < 0
+
+
+
 class Ship:
     COOLDOWN = 30
     def __init__(self, x, y, health=100):
@@ -41,9 +56,6 @@ class Ship:
             laser.move(vel)
             if laser.off_screen(HEIGHT):
                 self.lasers.remove(laser)
-            elif laser.collision(obj):
-                obj.health -= 10
-                self.lasers.remove(laser)
     def get_width(self):
         return self.ship_img.get_width()
     def get_height(self):
@@ -53,6 +65,11 @@ class Ship:
             self.cool_down_counter = 0
         elif self.cool_down_counter > 0:
             self.cool_down_counter += 1
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 
 
@@ -69,11 +86,6 @@ class Player(Ship):
             laser.move(vel)
             if laser.off_screen(HEIGHT):
                 self.lasers.remove(laser)
-            else:
-                 for obj in objs:
-                    if laser.collision(obj):
-                        objs.remove(obj)
-                        self.lasers.remove(laser)
     def healthbar(self, window):
         pygame.draw.rect(window, (255, 0, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 15))
         pygame.draw.rect(window, (0, 255, 0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health / self.max_health), 15))
@@ -95,6 +107,7 @@ class Enemy(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
     def move(self, vel):
         self.y += vel
+
 
 
 
@@ -159,14 +172,19 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player.get_height() + 20 + player_vel <= HEIGHT:
             player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
             
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
+            if random.randrange(0, 100) == 1:
+                enemy.shoot()
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
+        player.move_lasers(-laser_vel, enemies)
     
-
 
 
 main()
